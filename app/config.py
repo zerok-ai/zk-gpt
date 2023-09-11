6a6fa9ed4ebf1a1c704f90f19afcf65161fa54b2
CHNG7 <nikhilchinta7@gmail.com>
2023-09-11 11:42:22 +0530

    helm chamnges

--- app/config.py
+++ app/config.py
@@ -1,20 +1,21 @@
 import yaml
 import os
 import requests
-import schedule
 import time
-
 
 class Config:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.config_data = self._load_config()
         self.secrets = None
-
+        self._fetch_secrets_with_retry()
+        self.config_data = self._load_config()
+        
     def _load_config(self):
         try:
             with open(self.file_path, 'r') as file:
-                return yaml.safe_load(file)
+                config_dict = yaml.safe_load(file)
+                config_dict.update(self.secrets['payload'])
+                return config_dict
         except Exception as e:
             raise ValueError(f"Error loading config file: {e}")
 
@@ -23,7 +24,6 @@ class Config:
         env_value = os.environ.get(key)
         if env_value:
             return env_value
-        
         # If not found in environment variables, check if it exists in config data
         # If the key is not found in either config data or env, return the default value
         return self.config_data.get(key, default)
@@ -43,28 +43,28 @@ class Config:
             print(f"Secrets Fetch failed with error: {str(e)}")
             return None
 
-    def fetch_secrets_with_retry(self, max_retries=5, base_timeout=1):
+    def _fetch_secrets_with_retry(self):
+        max_retries=5
+        base_timeout=1
         retries = 0
         while retries < max_retries:
-            secrets = self.fetch_secrets_from_client_service()
-            if secrets:
-                self.secrets = secrets  # Store the secrets in the class instance
-                break
-
+            response = self.fetch_secrets_from_server()
+            if response:
+                self.secrets = response
+                return
             # Exponential backoff, increase the retry timeout exponentially
             retries += 1
             sleep_duration = base_timeout * (2 ** retries)
-            print(f"Retrying in {sleep_duration} seconds...")
+            print(f"Retrying in {sleep_duration} seconds... for fetching the secrets")
             time.sleep(sleep_duration)
+        if self.secrets is None:
+            raise Exception("Unable to fetch zk-llm Secrets from Server") 
+
 
     def job(self):
         secrets = self.fetch_secrets_with_retry()
         if secrets:
             # Use secrets in your service or store them in the class instance
             self.secrets = secrets
-    
-
 
 configuration = Config("config/config.yaml")
-
-
