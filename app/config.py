44ef5d0e6f3bc07b53ed71504e7f7e50de2f48cc
CHNG7 <nikhilchinta7@gmail.com>
2023-11-01 12:35:52 +0530

    config map changes

--- app/config.py
+++ app/config.py
@@ -103,4 +103,4 @@ class Config:
             raise ValueError(f"Error while fetching cluster id: {e}")
 
 
-configuration = Config("./../zk-gpt/config/config.yaml")
+configuration = Config("/config/config.yaml")
